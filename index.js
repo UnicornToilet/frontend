527b959e6049d234356bd893603859f3c4917353
Kyle Richey <richey.kyle.i@gmail.com>
2018-02-03 14:49:02 -0800

    added to package

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let express = require('express');
 let morgan = require('morgan');
 
+let PORT = 8080;
+
 let app = express();
 
 app.use(morgan('common'));
@@ -8,6 +10,6 @@ app.use(morgan('common'));
 app.use(express.static(`${__dirname}/build`));
 app.use('*', (req,res) => res.send(`${__dirname}/build/index.html`));
 
-app.listen(process.env.PORT, () => {
-    console.log("Server up on port", process.env.PORT);
+app.listen(PORT, () => {
+    console.log("Server up on port", PORT);
 });
