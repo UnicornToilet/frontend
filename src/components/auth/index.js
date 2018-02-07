5716a0d30941c13d7b66fbdb35eaee437161511e
Kyle Richey <richey.kyle.i@gmail.com>
2018-02-06 20:18:35 -0800

    syntax errors, re working add toilet form

--- src/components/auth/index.js
+++ src/components/auth/index.js
@@ -7,51 +7,48 @@ import AuthForm from './form';
 
 class Auth extends React.Component {
 
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    componentWillMount() {
-        this.props.authLogin();
-    }
+  componentWillMount() {
+    this.props.authLogin();
+  }
 
-    render() {
+  render() {
 
-        // using the allowLogin property to let components that want to be
-        // authorized to act as a simple Hide/Show or let this auth form take over
-        // their output
-        let form = this.props.allowLogin
-            ? <AuthForm handleCreate={this.props.authCreate} handleLogin={this.props.authLogin} />
-            : null
+    let form = this.props.allowLogin
+      ? <AuthForm handleCreate={this.props.authCreate} handleLogin={this.props.authLogin} />
+      : null;
 
-        return (
+    return (
 
-            <React.Fragment>
-                {
-                    renderIf (
-                        this.props.auth,
-                        this.props.children,
-                        form
-                    )
-                }
-            </React.Fragment>
+      <React.Fragment>
+        {
+          renderIf (
+            this.props.auth,
+            this.props.children,
+            form
+          )
+        }
+      </React.Fragment>
 
-        )
+    );
 
-    }
+  }
 
 }
 
 
 
 const mapStateToProps = (state) => ({
-    auth:state.auth
+  auth:state.auth,
 });
 
 const mapDispatchToProps = (dispatch, getState) => ({
-   authLogin: user => dispatch(actions.authLogin(user)),
-   authCreate: user => dispatch(actions.authCreateAccount(user)),
-   authLogout: () => dispatch(actions.authLogout()),
+  authLogin: user => dispatch(actions.authLogin(user)),
+  authCreate: user => dispatch(actions.authCreateAccount(user)),
+  authLogout: () => dispatch(actions.authLogout()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Auth);
--- src/components/google-map/add-toilet.js
+++ src/components/google-map/add-toilet.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Form, Text, Radio, RadioGroup} from 'react-form';
 
 const emptyState = {
   locationName: '',
@@ -20,9 +19,9 @@ class AddToilet extends React.Component{
   }
 
   handleChange(e){
-    let {field, value} = e.target;
+    let {name, value} = e.target;
     console.log(e.target);
-    this.setState({field:value});
+    this.setState({name:value});
   }
 
   handleSubmit(e){
@@ -34,94 +33,26 @@ class AddToilet extends React.Component{
   render(){
     return (
       <div>
-        <Form onSubmit={this.handleSubmit}>
-          { formApi => (
-            <form onSubmit={formApi.submitForm} id="form2">
-              <label htmlFor="locationName">Location Name</label>
-              <Text field="locationName" id="locationName" />
+        <form className='addToilet-form'>
 
-              <RadioGroup field="occupancy">
-                { group => (
-                  <div>
-                    <p>Occupany</p>
-                    <label htmlFor="single" className="mr-2">Single</label>
-                    <Radio group={group} value="single" id="single" className="mr-3 d-inline-block" />
+          <input type="text" id='toiletLocation' name='locationName' />
 
-                    <label htmlFor="multiple" className="mr-2">Multiple</label>
-                    <Radio group={group} value="multiple" id="multiple" className="d-inline-block" />
-                  </div>
-                )}
-              </RadioGroup>
-
-              <RadioGroup field="soap">
-                { group => (
-                  <div>
-                    <p>Soap Type</p>
-                    <label htmlFor="foam" className="mr-2">Foam</label>
-                    <Radio group={group} value="foam" id="foam" className="mr-3 d-inline-block" />
-
-                    <label htmlFor="gel" className="mr-2">Gel</label>
-                    <Radio group={group} value="gel" id="gel" className="d-inline-block" />
-                  </div>
-                )}
-              </RadioGroup>
-
-              <RadioGroup field="tpQuality">
-                { group => (
-                  <div>
-                    <p>Toilet Paper Quality</p>
-                    <label htmlFor="one" className="mr-2">1</label>
-                    <Radio group={group} value="one" id="one" className="mr-3 d-inline-block" />
-
-                    <label htmlFor="two" className="mr-2">2</label>
-                    <Radio group={group} value="two" id="two" className="d-inline-block" />
-
-                    <label htmlFor="three" className="mr-2">3</label>
-                    <Radio group={group} value="three" id="three" className="d-inline-block" />
-
-                    <label htmlFor="four" className="mr-2">4</label>
-                    <Radio group={group} value="four" id="four" className="d-inline-block" />
-
-                    <label htmlFor="five" className="mr-2">5</label>
-                    <Radio group={group} value="five" id="five" className="d-inline-block" />
-                  </div>
-                )}
-              </RadioGroup>
-
-              <RadioGroup field="drying">
-                { group => (
-                  <div>
-                    <p>Drying Method</p>
-                    <label htmlFor="towel" className="mr-2">Paper Towel</label>
-                    <Radio group={group} value="towel" id="towel" className="mr-3 d-inline-block" />
-
-                    <label htmlFor="air" className="mr-2">Air Dry</label>
-                    <Radio group={group} value="air" id="air" className="d-inline-block" />
-
-                    <label htmlFor="airTowel" className="mr-2">Both</label>
-                    <Radio group={group} value="airTowel" id="airTowel" className="d-inline-block" />
-                  </div>
-                )}
-              </RadioGroup>
-
-              <RadioGroup field="babyChanging">
-                { group => (
-                  <div>
-                    <p>Has Baby Changing Station</p>
-                    <label htmlFor="yesBaby" className="mr-2">Yes</label>
-                    <Radio group={group} value="yesBaby" id="yesBaby" className="mr-3 d-inline-block" />
-
-                    <label htmlFor="noBaby" className="mr-2">No</label>
-                    <Radio group={group} value="noBaby" id="noBaby" className="d-inline-block" />
-                  </div>
-                )}
-              </RadioGroup>
+          <h2>Toiet Paper Quality</h2>
+          <input type='radio' id='tpQuality1' name='tpQuality' value='1'/>
+          <label htmlFor='tpQuality1'> 1 </label>
+          <input type='radio' id='tpQuality2' name='tpQuality' value='2'/>
+          <label htmlFor='tpQuality2'> 2 </label>
+          <input type='radio' id='tpQuality3' name='tpQuality' value='3'/>
+          <label htmlFor='tpQuality3'> 3 </label>
+          <input type='radio' id='tpQuality4' name='tpQuality' value='4'/>
+          <label htmlFor='tpQuality4'> 4 </label>
+          <input type='radio' id='tpQuality5' name='tpQuality' value='5'/>
+          <label htmlFor='tpQuality5'> 5 </label>
 
 
-              <button type="submit" className="mb-4 btn btn-primary">Flush</button>
-            </form>
-          )}
-        </Form>
+
+          <button type='submit' onSubmit={this.handleSubmit}>Flush!</button>
+        </form>
       </div>
     );
   }
