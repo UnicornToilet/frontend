7f98e0e32b7fe8215153b364a45d6aa7a8791e46
Kyle Richey <richey.kyle.i@gmail.com>
2018-02-06 20:32:38 -0800

    removed add toilet from app

--- src/components/app.js
+++ src/components/app.js
@@ -8,7 +8,7 @@ import Header from './header';
 import Footer from './footer';
 import Map from '../components/google-map/map.js';
 import FilterForm from '../components/google-map/filter-form.js';
-import AddToilet from '../components/google-map/add-toilet.js';
+//import AddToilet from '../components/google-map/add-toilet.js';
 import Auth from './auth/index.js';
 
 class App extends React.Component {
@@ -23,7 +23,6 @@ class App extends React.Component {
                 <Header appTitle="Unicorn Toilet" />
                 <Auth />
 
-                //<AddToilet />
 
                 <main>
                     <Route exact path='/' component={Map} />
