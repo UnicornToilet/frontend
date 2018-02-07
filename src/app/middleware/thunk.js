e9e67c151186c84431b91e8b05ba3fd5b41edf1c
Kyle Richey <richey.kyle.i@gmail.com>
2018-02-06 23:28:33 -0800

    re factored addtoilet

--- src/app/middleware/thunk.js
+++ src/app/middleware/thunk.js
@@ -1,8 +1,8 @@
 let thunk = store => next => action => {
 
-    return typeof action === "function"
-        ? action(store.dispatch, store.getState)
-        : next(action);
+  return typeof action === 'function'
+    ? action(store.dispatch, store.getState)
+    : next(action);
 
 };
 
--- src/components/google-map/add-toilet.js
+++ src/components/google-map/add-toilet.js
@@ -49,7 +49,31 @@ class AddToilet extends React.Component{
           <input type='radio' id='tpQuality5' name='tpQuality' value='5'/>
           <label htmlFor='tpQuality5'> 5 </label>
 
+          <h2>Occupancy</h2>
+          <input type='radio' id='single' name='occupancy' value='single'/>
+          <label htmlFor='single'>Single</label>
+          <input type='radio' id='multiple' name='occupancy' value='multiple'/>
+          <label htmlFor='multiple'>Multiple</label>
 
+          <h2>Soap Type</h2>
+          <input type='radio' id='foam' name='soap' value='foam'/>
+          <label htmlFor='foam'>Foam</label>
+          <input type='radio' id='gel' name='soap' value='gel'/>
+          <label htmlFor='gel'>Gel</label>
+
+          <h2>Drying Method</h2>
+          <input type='radio' id='air' name='drying' value='air'/>
+          <label htmlFor='air'>Air Dry</label>
+          <input type='radio' id='towel' name='drying' value='towel'/>
+          <label htmlFor='towel'>Paper Towel</label>
+          <input type='radio' id='airTowel' name='drying' value='both'/>
+          <label htmlFor='airTowel'>Both</label>
+
+          <h2>Has Baby Changing Station</h2>
+          <input type='radio' id='yesBaby' name='babyChanging' value='hasBaby'/>
+          <label htmlFor='yesBaby'>True</label>
+          <input type='radio' id='noBaby' name='babyChanging' value='doesntHaveBaby'/>
+          <label htmlFor='noBaby'>False</label>
 
           <button type='submit' onSubmit={this.handleSubmit}>Flush!</button>
         </form>
