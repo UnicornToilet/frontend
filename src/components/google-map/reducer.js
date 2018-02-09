8a64cf1a589bc6be4e73cd5f08365aea58dd3d9b
Ashkaan Jaberi <ashkaan14@gmail.com>
2018-02-08 17:51:03 -0800

    fixing initial state

--- src/components/google-map/reducer.js
+++ src/components/google-map/reducer.js
@@ -3,7 +3,7 @@ const initialState = {};
 export default (state=initialState, {type,payload}) => {
   switch(type) {
     case 'FETCH':
-      return payload;
+      return payload || initialState;
 
     case 'ADD':
       return {...state, payload};
